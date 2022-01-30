4c11fe9d78b4ccb4e5c6f41bbcf514eb27f4c13b
Ntshembo Hlongwane <ntshembohlongwane1@gmail.com>
2022-01-30 15:10:57 +0200

    Add: Client side

---
+++ client/pages/index.tsx
@@ -0,0 +1,25 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>UNI CONENCT</title>
+        <meta
+          name="description"
+          content="his application is for simplifying the process of applying to universities for students"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>UNI CONNECT</h1>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
